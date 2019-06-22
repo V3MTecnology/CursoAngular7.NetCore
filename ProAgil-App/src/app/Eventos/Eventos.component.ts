0f6323c63347223a5aaa5bf50575bd00e75f4d73
Martino Morais <V3MTecnology@gmail.com>
2019-06-22 14:42:25 -0300

    Enviando paara github até a aula 66 validação de formulários react form

--- ProAgil-App/src/app/Eventos/Eventos.component.ts
+++ ProAgil-App/src/app/Eventos/Eventos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../_services/evento.service';
 import { Evento } from '../_models/Evento';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,53 +11,80 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap';
   styleUrls: ['./Eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-
+  
   eventosFiltrados: Evento[];
   eventos: Evento[] = [];
   imagemLargura = 50;
   imagemMargim = 2;
   mostrarImagem = false;
   modalRef: BsModalRef;
-
+  registerForm: FormGroup;
+  
   _filtroLista = '';
-
+  
   constructor(
     private eventoService: EventoService
-  , private modalService: BsModalService
+    , private modalService: BsModalService
     )  {  }
-
+    
     get filtroLista(): string {
       return this._filtroLista;
     }
     set filtroLista(value: string) {
       this._filtroLista = value;
-
+      
       this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
     }
-
+    
     openModal(template: TemplateRef<any>){
       this.modalRef = this.modalService.show(template);
-
+      
     }
-
-
+    
     ngOnInit() {
+      this.validation();
       this.getEventos();
     }
-
-
+    
+    
     filtrarEventos(filtrarPor: string): Evento[] {
       filtrarPor = filtrarPor.toLocaleLowerCase();
       return this.eventos.filter(
         evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
         );
       }
-
+      
       alternarImagem()
       {
         this.mostrarImagem = !this.mostrarImagem;
       }
 
+      validation()
+      {
+        this.registerForm = new FormGroup({
+          tema: new FormControl('',
+            [Validators.required, Validators.minLength(4), Validators.maxLength(50)]
+          ),
+          local: new FormControl('', Validators.required),
+          dataEvento: new FormControl('', Validators.required),
+          qtdPessoas: new FormControl('',
+              [Validators.required, Validators.maxLength(120000)]
+            ),
+          imagemURL: new FormControl('',
+              [Validators.required, Validators.email]
+              ),
+          telefone: new FormControl('', Validators.required),
+          email: new FormControl('', Validators.required)
+        });
+      }
+
+      salvarAlteracao()
+      {
+
+      }
+
+
+      
       getEventos()
       {
         this.eventoService.getAllEvento().subscribe(
@@ -64,7 +92,7 @@ export class EventosComponent implements OnInit {
             this.eventos =_eventos;
             this.eventosFiltrados = this.eventos;
             console.log(_eventos);
-
+            
           }, error => {
             console.log(error);
           } );
--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { TooltipModule, BsDropdownModule, ModalModule} from 'ngx-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -30,7 +30,8 @@ import { DateTimeFormatPipePipe } from './_helpes/DateTimeFormatPipe.pipe';
       ModalModule.forRoot(),
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      ReactiveFormsModule
    ],
    providers: [
       EventoService
